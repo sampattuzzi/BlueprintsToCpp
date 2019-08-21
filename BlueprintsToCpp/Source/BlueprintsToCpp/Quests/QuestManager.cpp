0e3b1b1f09b7c9507dd6c3086b5bb9c05a5c20fe
Samuel Pattuzzi <sam@pattuzzi.it>
2019-08-21 15:13:42 +0100

    Creating C++ Base Classes - Part 1

---
+++ BlueprintsToCpp/Source/BlueprintsToCpp/Quests/QuestManager.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "QuestManager.h"
+
+// Sets default values
+AQuestManager::AQuestManager()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AQuestManager::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AQuestManager::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
