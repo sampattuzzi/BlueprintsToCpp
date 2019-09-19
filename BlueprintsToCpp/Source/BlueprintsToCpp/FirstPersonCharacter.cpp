6eed9576588606c90a0890b8c92d0058927882ef
Samuel Pattuzzi <sam@pattuzzi.it>
2019-09-19 17:36:25 +0100

    Convert most function, expose grabber from Blueprint.

--- BlueprintsToCpp/Source/BlueprintsToCpp/FirstPersonCharacter.cpp
+++ BlueprintsToCpp/Source/BlueprintsToCpp/FirstPersonCharacter.cpp
@@ -3,27 +3,11 @@
 
 #include "FirstPersonCharacter.h"
 #include "Components/InputComponent.h"
+#include "GameFramework/CharacterMovementComponent.h"
 
 // Sets default values
 AFirstPersonCharacter::AFirstPersonCharacter()
 {
- 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
-
-}
-
-// Called when the game starts or when spawned
-void AFirstPersonCharacter::BeginPlay()
-{
-	Super::BeginPlay();
-	
-}
-
-// Called every frame
-void AFirstPersonCharacter::Tick(float DeltaTime)
-{
-	Super::Tick(DeltaTime);
-
 }
 
 // Called to bind functionality to input
@@ -32,15 +16,25 @@ void AFirstPersonCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInp
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
 
 	PlayerInputComponent->BindAxis(TEXT("Forward"), this, &AFirstPersonCharacter::Forward);
-	PlayerInputComponent->BindAction(TEXT("Jump"), EInputEvent::IE_Pressed, this, &AFirstPersonCharacter::Jump);
+	PlayerInputComponent->BindAxis(TEXT("LookUp"), this, &ACharacter::AddControllerPitchInput);
+	PlayerInputComponent->BindAxis(TEXT("LookRight"), this, &ACharacter::AddControllerYawInput);
+	PlayerInputComponent->BindAction(TEXT("Jump"), EInputEvent::IE_Pressed, this, &ACharacter::Jump);
+	PlayerInputComponent->BindAction(TEXT("Grab"), EInputEvent::IE_Pressed, this, &AFirstPersonCharacter::Grab);
+	PlayerInputComponent->BindAction(TEXT("Grab"), EInputEvent::IE_Released, this, &AFirstPersonCharacter::Release);
 }
 
 void AFirstPersonCharacter::Forward(float AxisValue)
 {
-	UE_LOG(LogTemp, Warning, TEXT("Forward %f!"), AxisValue);
+	GetCharacterMovement()->AddInputVector(GetActorForwardVector() * AxisValue);
 }
 
-void AFirstPersonCharacter::Jump()
+void AFirstPersonCharacter::Grab()
 {
-	UE_LOG(LogTemp, Warning, TEXT("Jump!"));
+	GetGrabber()->Grab();
 }
+
+void AFirstPersonCharacter::Release()
+{
+	GetGrabber()->Release();
+}
+
--- BlueprintsToCpp/Source/BlueprintsToCpp/FirstPersonCharacter.h
+++ BlueprintsToCpp/Source/BlueprintsToCpp/FirstPersonCharacter.h
@@ -4,30 +4,25 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Character.h"
+#include "Grabber.h"
 #include "FirstPersonCharacter.generated.h"
 
-UCLASS()
+UCLASS() //Note Blueprintable is inherited
 class BLUEPRINTSTOCPP_API AFirstPersonCharacter : public ACharacter
 {
 	GENERATED_BODY()
 
 public:
-	// Sets default values for this character's properties
 	AFirstPersonCharacter();
-
-protected:
-	// Called when the game starts or when spawned
-	virtual void BeginPlay() override;
-
-public:	
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
-
-	// Called to bind functionality to input
+	
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+protected:	
+	UFUNCTION(BlueprintImplementableEvent, BlueprintPure /*=>BlueprintCallable*/)
+	UGrabber* GetGrabber() const;
 
 private:
 	void Forward(float AxisValue);
-	void Jump();
-
+	void Grab();
+	void Release();
 };
