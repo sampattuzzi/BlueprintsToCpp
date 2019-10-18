d55e478d2b23d47239ad046121933016c5ffceaa
Samuel Pattuzzi <sam@pattuzzi.it>
2019-10-18 15:31:15 +0100

    03 Creating C++ Base Classes

---
+++ BlueprintsToCpp/Source/BlueprintsToCpp/BlueprintsToCpp.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "BlueprintsToCpp.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, BlueprintsToCpp, "BlueprintsToCpp" );
---
+++ BlueprintsToCpp/Source/BlueprintsToCpp/Grabber.h
@@ -0,0 +1,28 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/SceneComponent.h"
+#include "Grabber.generated.h"
+
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent), Blueprintable )
+class BLUEPRINTSTOCPP_API UGrabber : public USceneComponent
+{
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this component's properties
+	UGrabber();
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+		
+};
