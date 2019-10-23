692ea4476720f9be2f5d96888634a1ca331ae949
Samuel Pattuzzi <sam@pattuzzi.it>
2019-10-23 17:04:06 +0100

    06 UPROPERTY: Exposing Variables

--- BlueprintsToCpp/Source/BlueprintsToCpp/Grabber.h
+++ BlueprintsToCpp/Source/BlueprintsToCpp/Grabber.h
@@ -24,5 +24,7 @@ public:
 	// Called every frame
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 
-		
+protected:
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	float MaxGrabDistance = 100;
 };
