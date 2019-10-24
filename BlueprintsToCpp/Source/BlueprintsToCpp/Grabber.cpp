77fe9c043ccc14a3a6d1a4a640056211d75119ea
Samuel Pattuzzi <sam@pattuzzi.it>
2019-10-24 12:10:10 +0100

    08 Mapping Blueprint To C++

--- BlueprintsToCpp/Source/BlueprintsToCpp/Grabber.cpp
+++ BlueprintsToCpp/Source/BlueprintsToCpp/Grabber.cpp
@@ -4,6 +4,7 @@
 #include "Grabber.h"
 
 #include "Kismet/KismetMathLibrary.h"
+#include "GameFramework/Actor.h"
 
 // Sets default values for this component's properties
 UGrabber::UGrabber()
@@ -36,5 +37,15 @@ void UGrabber::TickComponent(float DeltaTime, ELevelTick TickType, FActorCompone
 
 FVector UGrabber::GetMaxGrabLocation() const
 {
-	return GetComponentLocation() + UKismetMathLibrary::GetForwardVector(GetComponentRotation()) * MaxGrabDistance;
+	return GetComponentLocation() + GetComponentRotation().Vector() * MaxGrabDistance;
+}
+
+FVector UGrabber::GetHoldLocation() const
+{
+	return GetComponentLocation() + GetComponentRotation().Vector() * HoldDistance;
+}
+
+UPhysicsHandleComponent* UGrabber::GetPhysicsComponent() const
+{
+	return GetOwner()->FindComponentByClass<UPhysicsHandleComponent>();
 }
--- BlueprintsToCpp/Source/BlueprintsToCpp/Grabber.h
+++ BlueprintsToCpp/Source/BlueprintsToCpp/Grabber.h
@@ -4,6 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "Components/SceneComponent.h"
+#include "PhysicsEngine/PhysicsHandleComponent.h"
 #include "Grabber.generated.h"
 
 
@@ -23,6 +24,12 @@ protected:
 	UFUNCTION(BlueprintCallable, BlueprintPure)
 	FVector GetMaxGrabLocation() const;
 
+	UFUNCTION(BlueprintCallable, BlueprintPure)
+	FVector GetHoldLocation() const;
+
+	UFUNCTION(BlueprintCallable, BlueprintPure)
+	UPhysicsHandleComponent* GetPhysicsComponent() const;
+
 public:	
 	// Called every frame
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
@@ -30,4 +37,7 @@ public:
 protected:
 	UPROPERTY(EditAnywhere, BlueprintReadOnly)
 	float MaxGrabDistance = 100;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	float HoldDistance = 100;
 };
