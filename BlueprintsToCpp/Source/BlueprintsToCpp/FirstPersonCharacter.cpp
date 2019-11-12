5ac9b7c152df373a9dc4ad3d0d466bb23bbfd124
Samuel Pattuzzi <sam@pattuzzi.it>
2019-11-12 16:13:19 +0000

    17 Binding user input

---
+++ BlueprintsToCpp/Source/BlueprintsToCpp/FirstPersonCharacter.cpp
@@ -0,0 +1,46 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "FirstPersonCharacter.h"
+#include "Components/InputComponent.h"
+
+// Sets default values
+AFirstPersonCharacter::AFirstPersonCharacter()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AFirstPersonCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AFirstPersonCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void AFirstPersonCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+	PlayerInputComponent->BindAxis(TEXT("Forward"), this, &AFirstPersonCharacter::Forward);
+	PlayerInputComponent->BindAction(TEXT("Jump"), EInputEvent::IE_Pressed, this, &AFirstPersonCharacter::Jump);
+
+}
+
+void AFirstPersonCharacter::Forward(float AxisValue)
+{
+	UE_LOG(LogTemp, Warning, TEXT("Forward %f"), AxisValue);
+}
+
+void AFirstPersonCharacter::Jump()
+{
+	UE_LOG(LogTemp, Warning, TEXT("Jump"));
+}
